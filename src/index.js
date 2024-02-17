c228c69d556f1eb6ffc3da23f0c6199e2df1f00e
rod773 <104315743+rod773@users.noreply.github.com>
2024-02-17 11:39:39 +0100

    initial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "../node_modules/semantic-ui/dist/semantic.min.css";
+import "../node_modules/semantic-ui/dist/semantic.min";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );

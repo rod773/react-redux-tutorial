56025432f16c79d705cee1535c47e2541bb9a9b0
rod773 <104315743+rod773@users.noreply.github.com>
2024-02-18 10:28:38 +0100

    update

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const ProductComponent = () => {
   const products = useSelector((state) => state.allProducts.products);
@@ -26,12 +27,14 @@ const ProductComponent = () => {
           </p>
         </div>
         <div className="p-6 pt-0">
-          <button
-            className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
-            type="button"
-          >
-            Read More
-          </button>
+          <Link to={`/product/${id}`}>
+            <button
+              className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+              type="button"
+            >
+              Read More
+            </button>
+          </Link>
         </div>
       </div>
     );
